6b81e5876dae31de7f729796a8052a6ba903b2b6
tonydpadua <tonydpadua1999@gmail.com>
2021-01-05 18:19:37 -0300

    Exceptions adjustment

--- src/main/java/com/tony/pandemic/exception/RestControllerExceptionHandler.java
+++ src/main/java/com/tony/pandemic/exception/RestControllerExceptionHandler.java
@@ -13,14 +13,25 @@ import javax.servlet.http.HttpServletRequest;
 public class RestControllerExceptionHandler {
 
     @ExceptionHandler(ObjectNotFoundException.class)
-    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e) {
         StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+    @ExceptionHandler(EmptyException.class)
+    public ResponseEntity<StandardError> empty(ObjectNotFoundException e) {
+        StandardError err = new StandardError(HttpStatus.NOT_ACCEPTABLE.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(err);
+    }
 
+    @ExceptionHandler(InvalidNegotiationException.class)
+    public ResponseEntity<StandardError> invalidNegotiation(ObjectNotFoundException e) {
+        StandardError err = new StandardError(HttpStatus.NOT_ACCEPTABLE.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e) {
         ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Validation error", System.currentTimeMillis());
         for (FieldError x : e.getBindingResult().getFieldErrors()) {
             err.addError(x.getField(), x.getDefaultMessage());

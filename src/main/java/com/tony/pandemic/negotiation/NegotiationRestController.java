c317171a517f079ca7ff93300a8c62ce05416355
tonydpadua <tonydpadua1999@gmail.com>
2021-01-05 20:51:18 -0300

    Fixing negotiation

--- src/main/java/com/tony/pandemic/negotiation/NegotiationRestController.java
+++ src/main/java/com/tony/pandemic/negotiation/NegotiationRestController.java
@@ -2,10 +2,12 @@ package com.tony.pandemic.negotiation;
 
 import com.tony.pandemic.negotiation.involved.InvolvedHospitalDTO;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @AllArgsConstructor
 @Controller
@@ -15,6 +17,7 @@ public class NegotiationRestController {
     private INegotiationService service;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.OK)
     public void negotiationHospitals(@RequestBody InvolvedHospitalDTO[] involvedHospitals) {
         this.service.negotiationHospitals(involvedHospitals[0], involvedHospitals[1]);
     }
